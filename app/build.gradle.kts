c94989b60b7ee50182e4c671fb066ee075129f76
Gabriel Fontán <gabilessto@gmail.com>
2022-10-17 16:36:59 +0200

    Initial commit (started setting up project structure)

---
+++ app/build.gradle.kts
@@ -0,0 +1,184 @@
+import java.io.FileInputStream
+import java.util.Properties
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-kapt")
+    id("org.jetbrains.kotlin.android")
+    kotlin("plugin.serialization") version "1.7.10"
+}
+apply(plugin = "dagger.hilt.android.plugin")
+
+val versionMajor = 1
+val versionMinor = 0
+val versionPatch = 0
+val versionBuild = 0
+val isStable = false
+
+val composeVersion: String by rootProject.extra
+val lifecycleVersion: String by rootProject.extra
+val navigationVersion: String by rootProject.extra
+val roomVersion: String by rootProject.extra
+val accompanistVersion: String by rootProject.extra
+val composeMd3Version: String by rootProject.extra
+val coilVersion: String by rootProject.extra
+val okhttpVersion: String by rootProject.extra
+val hiltVersion: String by rootProject.extra
+
+val keystorePropertiesFile = rootProject.file("keystore.properties")
+
+val splitApks = !project.hasProperty("noSplits")
+
+android {
+    if (keystorePropertiesFile.exists()) {
+        val keystoreProperties = Properties()
+        keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+        signingConfigs {
+            getByName("debug")
+            {
+                keyAlias = keystoreProperties["keyAlias"].toString()
+                keyPassword = keystoreProperties["keyPassword"].toString()
+                storeFile = file(keystoreProperties["storeFile"]!!)
+                storePassword = keystoreProperties["storePassword"].toString()
+            }
+        }
+    }
+
+    compileSdk = 33
+    defaultConfig {
+        applicationId = "com.bobbyesp.spowlo"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 10000
+        versionName = StringBuilder("${versionMajor}.${versionMinor}.${versionPatch}").apply {
+            if (!isStable) append("-beta.${versionBuild}")
+            if (!splitApks) append("-(F-Droid)")
+        }.toString()
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        kapt {
+            arguments {
+                arg("room.schemaLocation", "$projectDir/schemas")
+            }
+        }
+        if (!splitApks)
+            ndk {
+                (properties["ABI_FILTERS"] as String).split(';').forEach {
+                    abiFilters.add(it)
+                }
+            }
+    }
+    if (splitApks)
+        splits {
+            abi {
+                isEnable = !project.hasProperty("noSplits")
+                reset()
+                include("arm64-v8a", "armeabi-v7a", "x86", "x86_64")
+                isUniversalApk = false
+            }
+        }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
+            if (keystorePropertiesFile.exists())
+                signingConfig = signingConfigs.getByName("debug")
+        }
+        debug {
+            if (keystorePropertiesFile.exists())
+                signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+
+    lint {
+        disable.addAll(listOf("MissingTranslation", "ExtraTranslation"))
+    }
+
+    applicationVariants.all {
+        outputs.all {
+            (this as com.android.build.gradle.internal.api.BaseVariantOutputImpl).outputFileName =
+                "Spowlo-${defaultConfig.versionName}-${name}.apk"
+        }
+    }
+
+    kotlinOptions {
+        freeCompilerArgs = freeCompilerArgs + "-opt-in=kotlin.RequiresOptIn"
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.3.0"
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+        jniLibs.useLegacyPackaging = true
+    }
+    namespace = "com.bobbyesp.spowlo"
+}
+
+dependencies {
+
+    implementation(project(":color"))
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.0-rc01")
+    implementation("com.google.android.material:material:1.8.0-alpha01")
+    implementation("androidx.activity:activity-compose:1.6.0")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
+
+    implementation("androidx.navigation:navigation-fragment-ktx:$navigationVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navigationVersion")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.material3:material3:$composeMd3Version")
+    implementation("androidx.compose.material3:material3-window-size-class:$composeMd3Version")
+    implementation("androidx.compose.material:material:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+    implementation("androidx.compose.material:material-icons-extended:$composeVersion")
+    implementation("androidx.navigation:navigation-compose:$navigationVersion")
+    implementation("androidx.compose.animation:animation-graphics:$composeVersion")
+    implementation("androidx.compose.foundation:foundation:$composeVersion")
+    implementation("com.google.accompanist:accompanist-navigation-animation:$accompanistVersion")
+    implementation("com.google.accompanist:accompanist-permissions:$accompanistVersion")
+    implementation("com.google.accompanist:accompanist-systemuicontroller:$accompanistVersion")
+    implementation("io.coil-kt:coil-compose:$coilVersion")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
+
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+    kapt("androidx.hilt:hilt-compiler:1.0.0")
+    implementation("com.google.dagger:hilt-android:$hiltVersion")
+    kapt("com.google.dagger:hilt-android-compiler:$hiltVersion")
+
+    implementation("androidx.room:room-runtime:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
+
+    // okhttp
+    implementation("com.squareup.okhttp3:okhttp:$okhttpVersion")
+
+
+    implementation("com.tencent:mmkv:1.2.14")
+
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
+    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
+}
---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+buildscript {
+    val composeVersion by extra("1.3.0-beta03")
+    val lifecycleVersion by extra("2.6.0-alpha02")
+    val navigationVersion by extra("2.5.2")
+    val roomVersion by extra("2.4.3")
+    val accompanistVersion by extra("0.25.1")
+    val composeMd3Version by extra("1.0.0-beta03")
+    val coilVersion by extra("2.2.1")
+    val okhttpVersion by extra("5.0.0-alpha.10")
+    val kotlinVersion by extra("1.7.10")
+    val hiltVersion by extra("2.44")
+
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${project.extra["kotlinVersion"]}")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:${project.extra["hiltVersion"]}")
+    }
+}
+plugins {
+    id("com.android.application") version "7.3.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.7.10" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
