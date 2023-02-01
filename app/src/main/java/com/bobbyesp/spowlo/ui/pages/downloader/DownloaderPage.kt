62f56b53133be6f72ad5263b48f6bcde3626e7f1
Gabriel Fontán <gabilessto@gmail.com>
2023-02-01 23:49:31 +0100

    Added the Downloader page and create the DownloadsUtil file

---
+++ app/src/main/java/com/bobbyesp/spowlo/ui/pages/downloader/DownloaderPage.kt
@@ -0,0 +1,11 @@
+package com.bobbyesp.spowlo.ui.pages.downloader
+
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@Composable
+@OptIn(ExperimentalAnimationApi::class, ExperimentalMaterial3Api::class)
+fun DownloaderPage(){
+
+}
---
+++ app/src/main/java/com/bobbyesp/spowlo/ui/pages/downloader/DownloaderViewModel.kt
@@ -0,0 +1,7 @@
+package com.bobbyesp.spowlo.ui.pages.downloader
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class DownloaderViewModel @Inject constructor() : ViewModel() {
+}
