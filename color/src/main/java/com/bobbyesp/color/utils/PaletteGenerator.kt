ebd72cd916835ad1cba5058932e208522284f995
Gabriel Fontán <gabilessto@gmail.com>
2023-08-01 17:50:14 +0200

    improve: Moved color declaration from string to `Color`

--- color/src/main/java/com/bobbyesp/color/utils/PaletteGenerator.kt
+++ color/src/main/java/com/bobbyesp/color/utils/PaletteGenerator.kt
@@ -3,6 +3,7 @@ package com.bobbyesp.color.utils
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
+import androidx.compose.ui.graphics.Color
 import androidx.palette.graphics.Palette
 import coil.ImageLoader
 import coil.request.ImageRequest
@@ -26,26 +27,26 @@ object PaletteGenerator {
         }
     }
 
-    fun extractColorsFromBitmap(bitmap: Bitmap): Map<ColorType, String> {
+    fun extractColorsFromBitmap(bitmap: Bitmap): Map<ColorType, Color> {
         return mapOf(
             ColorType.VIBRANT to parseColorSwatch(
-                color = Palette.from(bitmap).generate().vibrantSwatch
+                Palette.from(bitmap).generate().vibrantSwatch
             ),
             ColorType.DARK_VIBRANT to parseColorSwatch(
-                color = Palette.from(bitmap).generate().darkVibrantSwatch
+                Palette.from(bitmap).generate().darkVibrantSwatch
             ),
-            ColorType.ON_DARK_VIBRANT to parseBodyColor(
-                color = Palette.from(bitmap).generate().darkVibrantSwatch?.bodyTextColor
+            ColorType.ON_DARK_VIBRANT to parseColorSwatch(
+                Palette.from(bitmap).generate().darkVibrantSwatch
             )
         )
     }
 
-    private fun parseColorSwatch(color: Palette.Swatch?): String {
+    private fun parseColorSwatch(color: Palette.Swatch?): Color {
         return if (color != null) {
             val parsedColor = Integer.toHexString(color.rgb)
-            return "#$parsedColor"
+            return parsedColor.toColor()
         } else {
-            "#000000"
+            "#000000".toColor()
         }
     }
 
@@ -57,6 +58,10 @@ object PaletteGenerator {
             "#FFFFFF"
         }
     }
+}
+
+fun String.toColor(): Color {
+    return Color(android.graphics.Color.parseColor(this))
 }
 
 enum class ColorType {
