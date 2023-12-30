7b4ccc78198414a20fc78d19315958a379071e7d
Gabriel Fontán <gabilessto@gmail.com>
2023-12-30 01:36:18 +0100

    (ui): Improved app Theme.kt

---
+++ app/src/main/java/com/bobbyesp/spowlo/ui/theme/Shapes.kt
@@ -0,0 +1,5 @@
+package com.bobbyesp.spowlo.ui.theme
+
+import androidx.compose.material3.Shapes
+
+val Shapes = Shapes()
--- app/src/main/java/com/bobbyesp/spowlo/ui/theme/Theme.kt
+++ app/src/main/java/com/bobbyesp/spowlo/ui/theme/Theme.kt
@@ -3,17 +3,23 @@ package com.bobbyesp.spowlo.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.text.style.LineBreak
+import androidx.compose.ui.text.style.TextDirection
 import androidx.core.view.WindowCompat
+import com.kyant.monet.dynamicColorScheme
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -40,30 +46,28 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun SpowloTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    isHighContrastModeEnabled: Boolean = false,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-      dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-        val context = LocalContext.current
-        if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-      }
-      darkTheme -> DarkColorScheme
-      else -> LightColorScheme
+    val colorScheme =
+        dynamicColorScheme(!darkTheme).run {
+            if (isHighContrastModeEnabled && darkTheme) copy(
+                surface = Color.Black,
+                background = Color.Black,
+            )
+            else this
+        }
+    ProvideTextStyle(
+        value = LocalTextStyle.current.copy(
+            lineBreak = LineBreak.Paragraph,
+            textDirection = TextDirection.Content
+        )
+    ) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = Typography,
+            shapes = Shapes,
+            content = content
+        )
     }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-      SideEffect {
-        val window = (view.context as Activity).window
-        window.statusBarColor = colorScheme.primary.toArgb()
-        WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-      }
-    }
-
-    MaterialTheme(
-      colorScheme = colorScheme,
-      typography = Typography,
-      content = content
-    )
 }
