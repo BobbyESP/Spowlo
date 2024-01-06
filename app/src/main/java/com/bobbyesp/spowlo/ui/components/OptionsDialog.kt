ef57dc4f6aa4fc78b759bdfe7ec4c581a222e154
Gabriel Fontán <gabilessto@gmail.com>
2024-01-06 17:12:46 +0100

    Added app logo

--- app/src/main/java/com/bobbyesp/spowlo/ui/components/OptionsDialog.kt
+++ app/src/main/java/com/bobbyesp/spowlo/ui/components/OptionsDialog.kt
@@ -1,12 +1,57 @@
 package com.bobbyesp.spowlo.ui.components
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.bobbyesp.spowlo.R
+import com.bobbyesp.spowlo.ui.theme.SpowloTheme
+import com.bobbyesp.spowlo.ui.theme.unbounded
 
 @Composable
-fun OptionsDialog() {
-    Surface {
-        Text(text = "Options Dialog. I just want to see if this works.")
+fun OptionsDialog(
+    isPreview: Boolean = false
+) {
+    Surface(
+        modifier = Modifier.fillMaxWidth(),
+        tonalElevation = 12.dp,
+        shape = MaterialTheme.shapes.medium,
+        shadowElevation = 8.dp
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            //Icon(bitmap = , contentDescription = )
+            Text(
+                text = stringResource(id = R.string.app_name),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                style = MaterialTheme.typography.headlineLarge,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.SemiBold,
+                fontFamily = if(isPreview) null else unbounded
+            )
+        }
+    }
+}
+
+@Preview
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+@Composable
+private fun OptionsDialogPreview() {
+     SpowloTheme {
+         OptionsDialog(isPreview = true)
     }
 }
---
+++ app/src/main/java/com/bobbyesp/spowlo/ui/theme/Font.kt
@@ -0,0 +1,10 @@
+package com.bobbyesp.spowlo.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.bobbyesp.spowlo.R
+
+val unbounded = FontFamily(
+    Font(R.font.unbounded_medium, FontWeight.Medium),
+)
